c2f9286e0a0f3d424eb9396d77c726ca1d8d1eac
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-01 12:53:55 -0300

    feat: create cart page

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -2,12 +2,14 @@ import { createAppContainer } from 'react-navigation';
 import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import Main from './pages/Main';
+import Cart from './pages/Cart';
 import Header from './components/Header';
 
 const Routes = createAppContainer(
   createStackNavigator(
     {
       Main,
+      Cart,
     },
     {
       defaultNavigationOptions: {
