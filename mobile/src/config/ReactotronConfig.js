7fa7f4e5b90b71bc397af59940c91e8417cd1f6c
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-07 18:06:17 -0300

    feat: configure reactotron to see redux

--- mobile/src/config/ReactotronConfig.js
+++ mobile/src/config/ReactotronConfig.js
@@ -1,7 +1,11 @@
 import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (__DEV__) {
   const tron = Reactotron.configure()
+    .use(reactotronRedux())
+    .use(reactotronSaga())
     .useReactNative()
     .connect();
 
