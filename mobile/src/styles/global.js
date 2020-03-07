7b09f4307ffaa2f83598496595d82088c7ce037f
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-07 18:07:25 -0300

    feat: add pagename as a global style

---
+++ mobile/src/styles/global.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export const PageName = styled.Text`
+  font-size: 16px;
+  color: #cacaca;
+  padding-bottom: 10px;
+  margin-bottom: 0px;
+  border-bottom-width: 1px;
+  border-bottom-color: #cacaca;
+`;
