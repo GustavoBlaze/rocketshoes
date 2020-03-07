db2eb11b37ce032e17abfd9f1b44818ffff0d9e3
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-07 18:10:52 -0300

    feat: add cart page

--- mobile/src/pages/Cart/index.js
+++ mobile/src/pages/Cart/index.js
@@ -1,7 +1,137 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { ActivityIndicator } from 'react-native';
 
-import { Container } from './styles';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-export default function Cart() {
-  return <Container />;
+import { PageName } from '../../styles/global';
+
+import {
+  Container,
+  Card,
+  EmptyCartCard,
+  EmptyCartText,
+  ProductList,
+  Product,
+  ProductPreview,
+  ProductImage,
+  ProductInformation,
+  ProductName,
+  ProductPrice,
+  SubtotalContainer,
+  AmountContainer,
+  QuantityButton,
+  Input,
+  SubtotalText,
+  DeleteButton,
+  TotalContainer,
+  TotalHint,
+  TotalText,
+  Button,
+  ButtonText,
+} from './styles';
+
+import { formatPrice } from '../../util/format';
+import * as CartActions from '../../store/modules/cart/actions';
+
+function Cart({ cart, total, removeFromCart, updateAmountRequest, updating }) {
+  const increment = product => {
+    updateAmountRequest(product.id, product.amount + 1);
+  };
+
+  const decrement = product => {
+    updateAmountRequest(product.id, product.amount - 1);
+  };
+
+  if (cart.length === 0) {
+    return (
+      <Container>
+        <PageName>Carrinho</PageName>
+        <EmptyCartCard>
+          <Icon color="#cacaca" name="remove-shopping-cart" size={50} />
+          <EmptyCartText>Seu carrinho está vazio</EmptyCartText>
+        </EmptyCartCard>
+      </Container>
+    );
+  }
+  return (
+    <Container>
+      <PageName>Carrinho</PageName>
+      <Card>
+        <ProductList
+          data={cart}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item: product }) => (
+            <Product>
+              <ProductPreview>
+                <ProductImage source={{ uri: product.image }} />
+                <ProductInformation>
+                  <ProductName>{product.title}</ProductName>
+                  <ProductPrice>{product.priceFormatted}</ProductPrice>
+                </ProductInformation>
+                <DeleteButton onPress={() => removeFromCart(product.id)}>
+                  <Icon color="#7159c1" size={25} name="delete-forever" />
+                </DeleteButton>
+              </ProductPreview>
+              <SubtotalContainer>
+                <AmountContainer>
+                  <QuantityButton onPress={() => decrement(product)}>
+                    <Icon
+                      color="#7159c1"
+                      size={20}
+                      name="remove-circle-outline"
+                    />
+                  </QuantityButton>
+                  <Input value={String(product.amount)} />
+                  <QuantityButton onPress={() => increment(product)}>
+                    <Icon color="#7159c1" size={20} name="add-circle-outline" />
+                  </QuantityButton>
+                </AmountContainer>
+
+                {updating.id === product.id && updating.status ? (
+                  <ActivityIndicator color="#fff" size={20} />
+                ) : (
+                  <SubtotalText>{product.subtotal}</SubtotalText>
+                )}
+              </SubtotalContainer>
+            </Product>
+          )}
+        />
+        <TotalContainer>
+          <TotalHint>Total</TotalHint>
+          <TotalText>{total}</TotalText>
+        </TotalContainer>
+        <Button>
+          <ButtonText>finalizar pedido</ButtonText>
+        </Button>
+      </Card>
+    </Container>
+  );
 }
+
+const mapStateToProps = state => ({
+  cart: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
+  updating: state.updating,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+Cart.propTypes = {
+  cart: PropTypes.array.isRequired,
+  total: PropTypes.string.isRequired,
+  updating: PropTypes.object.isRequired,
+  removeFromCart: PropTypes.func.isRequired,
+  updateAmountRequest: PropTypes.func.isRequired,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- mobile/src/pages/Cart/styles.js
+++ mobile/src/pages/Cart/styles.js
@@ -1,3 +1,164 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  background: #191920;
+  flex: 1;
+  padding: 20px;
+  padding-top: 10px;
+  flex-direction: column;
+`;
+
+export const Card = styled.View`
+  flex-direction: column;
+  background: #fff;
+  padding: 10px;
+  border-radius: 4px;
+  margin-top: 12px;
+  max-height: 90%;
+`;
+
+export const EmptyCartCard = styled.View`
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  padding: 20px 0;
+  border-radius: 4px;
+  margin-top: 12px;
+  max-height: 90%;
+`;
+
+export const EmptyCartText = styled.Text`
+  margin-top: 10px;
+  font-weight: bold;
+  color: #cacaca;
+  font-size: 30px;
+`;
+
+export const ProductList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: true,
+})`
+  padding: 5px;
+  margin-bottom: 15px;
+`;
+
+export const Product = styled.View`
+  flex-direction: column;
+  width: 100%;
+  margin: 10px 0;
+`;
+
+export const ProductPreview = styled.View`
+  flex-direction: row;
+`;
+
+export const ProductImage = styled.Image.attrs({
+  resizeMode: 'cover',
+})`
+  width: 80px;
+  height: 80px;
+`;
+
+export const ProductInformation = styled.View`
+  flex-direction: column;
+  flex: 1;
+  padding-left: 10px;
+`;
+
+export const ProductName = styled.Text`
+  font-size: 14px;
+  color: #333;
+  line-height: 18px;
+  margin-bottom: 5px;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 16px;
+  line-height: 19px;
+  font-weight: bold;
+  color: #000;
+`;
+
+export const DeleteButton = styled.TouchableOpacity`
+  align-self: center;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+`;
+
+export const SubtotalContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background: #eee;
+  padding: 10px;
+  border-radius: 4px;
+`;
+
+export const AmountContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const QuantityButton = styled.TouchableOpacity`
+  padding: 2px;
+`;
+
+export const Input = styled.TextInput.attrs({
+  editable: false,
+})`
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  background: #fff;
+  padding: 5px;
+  width: 51px;
+  font-size: 14px;
+  line-height: 16px;
+  color: #666;
+  height: 26px;
+  text-align: center;
+  margin: 0 10px;
+`;
+
+export const SubtotalText = styled.Text`
+  color: #000;
+  font-size: 16px;
+  line-height: 19px;
+  font-weight: bold;
+`;
+
+export const TotalContainer = styled.View`
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+export const TotalHint = styled.Text`
+  font-weight: bold;
+  color: #999;
+  font-size: 16px;
+  text-transform: uppercase;
+  margin-bottom: 5px;
+`;
+
+export const TotalText = styled.Text`
+  font-weight: bold;
+  color: #000;
+  font-size: 30px;
+  text-transform: uppercase;
+`;
+
+export const Button = styled(RectButton)`
+  padding: 13px;
+  background: #7159c1;
+  border-radius: 4px;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 14px;
+  color: white;
+  font-weight: bold;
+  text-transform: uppercase;
+  text-align: center;
+`;
