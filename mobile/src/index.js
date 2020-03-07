fae665c88624ea6dc8da02671ed56c7fd9e88abe
Luiz Gustavo <gustavoblaze1524@gmail.com>
2020-03-07 18:08:19 -0300

    feat: add redux provider and store

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
 import './config/ReactotronConfig';
 import Routes from './routes';
+import NavigationService from './services/navigation';
+import store from './store';
 
-export default function src() {
+export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar barStyle="light-content" backgroundColor="#141419" />
-      <Routes />
-    </>
+      <Routes ref={ref => NavigationService.setNavigator(ref)} />
+    </Provider>
   );
 }
